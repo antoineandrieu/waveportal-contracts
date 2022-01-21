96dc37ac30f98de17b368ba4d5cf51a7e8da439b
Antoine <antoine@codingant.io>
2022-01-21 10:18:29 +0100

    feat: add lottery & cooldown

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -17,23 +18,57 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() payable {}
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
-	console.log("%s has waved!", msg.sender);
+        console.log("%s has waved!", msg.sender);
 
-	waves.push(Wave(msg.sender, _message, block.timestamp));
+        waves.push(Wave(msg.sender, _message, block.timestamp));
 
-	emit NewWave(msg.sender, block.timestamp, _message);
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
 
-	uint256 prizeAmount = 0.0001 ether;
-	require(
-	    prizeAmount <= address(this).balance,
-	    "Trying to withdraw more money than the contract has."
-	);
-	(bool success, ) = (msg.sender).call{value: prizeAmount}("");
-	require(success, "Failed to withdraw money from contract.");
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
@@ -41,7 +76,6 @@ contract WavePortal {
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
